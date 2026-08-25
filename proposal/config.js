d39ebc57856fbcab2b22038d9d5663190dcd708d
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-25 18:41:21 -0300

    chore: configure Supabase project

--- proposal/config.js
+++ proposal/config.js
@@ -1,6 +1,6 @@
 export const proposalConfig = Object.freeze({
-  supabaseUrl: '',
-  supabasePublishableKey: '',
+  supabaseUrl: 'https://orxenaynumsrohvsywxb.supabase.co',
+  supabasePublishableKey: 'sb_publishable_HUUvFXqWLb5ApCmi21uuWQ_nnirYlZd',
   publicProposalUrl: '/apresentacao/proposta/view.html',
   loginUrl: '/apresentacao/proposta/',
 });
