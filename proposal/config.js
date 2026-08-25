a0295b309a25f52b454917121ca04b677eeda134
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-25 18:13:17 -0300

    feat: add proposal runtime configuration

---
+++ proposal/config.js
@@ -0,0 +1,6 @@
+export const proposalConfig = Object.freeze({
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  publicProposalUrl: '/apresentacao/proposta/view.html',
+  loginUrl: '/apresentacao/proposta/',
+});
