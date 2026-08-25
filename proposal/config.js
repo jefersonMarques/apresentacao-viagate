73a11917dcd2dec8349312e98f889361bf9d00c5
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-25 19:14:35 -0300

    feat: add public presentation URL

--- proposal/config.js
+++ proposal/config.js
@@ -2,5 +2,6 @@ export const proposalConfig = Object.freeze({
   supabaseUrl: 'https://orxenaynumsrohvsywxb.supabase.co',
   supabasePublishableKey: 'sb_publishable_HUUvFXqWLb5ApCmi21uuWQ_nnirYlZd',
   publicProposalUrl: '/apresentacao/proposta/view.html',
+  publicPresentationUrl: '/apresentacao/view.html',
   loginUrl: '/apresentacao/proposta/',
 });
