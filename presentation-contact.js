03ad8faaa42346547e0a72134a9458e48306ba60
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-25 18:52:01 -0300

    feat: add configurable presentation sales contact

---
+++ presentation-contact.js
@@ -0,0 +1,8 @@
+window.presentationContact = Object.freeze({
+  name: 'Antônio Santos',
+  role: 'Sócio-Diretor Comercial',
+  email: 'antonio.santos@viagate.com.br',
+  phone: '(41) 99962-3600',
+  whatsapp: '5541999623600',
+  photoUrl: './assets/antonio-photo.svg',
+});
