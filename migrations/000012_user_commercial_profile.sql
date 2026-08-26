88b7de97db1437661cc8a779369d1c53674f6851
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-26 19:10:46 -0300

    Align user phone constraint with shared normalizer

--- migrations/000012_user_commercial_profile.sql
+++ migrations/000012_user_commercial_profile.sql
@@ -4,8 +4,4 @@ alter table users
 
 alter table users
   add constraint users_phone_raw_check
-  check (
-    phone is null
-    or phone = ''
-    or phone ~ '^(?:55)?[1-9][0-9]{9,10}$'
-  ) not valid;
+  check (phone is null or phone = '' or phone ~ '^([0-9]{10,11}|55[0-9]{10,11})$') not valid;
