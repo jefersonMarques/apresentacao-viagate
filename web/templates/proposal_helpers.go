a438e42f388b065b28325e3f6ace1aa25cdfac2b
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-26 11:25:06 -0300

    feat: add proposal editor template helpers

---
+++ web/templates/proposal_helpers.go
@@ -0,0 +1,13 @@
+package templates
+
+import "github.com/jefersonMarques/apresentacao-viagate/internal/proposals"
+
+func ProposalEditorItem(input proposals.EditorInput,catalogID string)(proposals.EditorItem,bool){
+	for _,item:=range input.Items{if item.CatalogID==catalogID{return item,true}}
+	return proposals.EditorItem{},false
+}
+
+func IsPricingModel(input proposals.EditorInput,model string)bool{
+	if input.PricingModel==""{return model=="per_item"}
+	return input.PricingModel==model
+}
