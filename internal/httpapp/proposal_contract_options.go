5c949592ae49d5454d7d546f92f065cb49ac7ec5
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-27 16:05:56 -0300

    Persist proposal contract assignment columns

--- internal/httpapp/proposal_contract_options.go
+++ internal/httpapp/proposal_contract_options.go
@@ -74,3 +74,28 @@ func (a *App) resolveProposalContractTemplateVersion(ctx context.Context, templa
 	}
 	return versionID, nil
 }
+
+func (a *App) persistProposalContractAssignment(ctx context.Context, draft proposals.SavedDraft, input proposals.EditorInput) error {
+	templateID := proposalContentString(input.Content, "proposal", "contract_template_id")
+	versionID := proposalContentString(input.Content, "proposal", "contract_template_version_id")
+	tx, err := a.pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+	if _, err := tx.Exec(ctx, `
+		update proposals
+		set contract_template_id=nullif($2,'')::uuid
+		where id=$1
+	`, draft.ProposalID, templateID); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(ctx, `
+		update proposal_versions
+		set contract_template_version_id=nullif($2,'')::uuid
+		where id=$1 and published_at is null
+	`, draft.VersionID, versionID); err != nil {
+		return err
+	}
+	return tx.Commit(ctx)
+}
