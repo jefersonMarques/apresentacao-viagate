1687328d9368a94df4b0d5f11af1f0c1bc19f648
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-31 13:56:48 -0300

    add customer signature receipt route

--- internal/httpapp/app.go
+++ internal/httpapp/app.go
@@ -127,6 +127,7 @@ func (a *App) Routes() http.Handler {
 	router.Post("/sign/{token}/otp", a.sendSignatureOTP)
 	router.Post("/sign/{token}/confirm", a.confirmSignature)
 	router.Get("/sign/{token}/contract", a.downloadSignedContract)
+	router.Get("/sign/{token}/receipt", a.downloadCustomerSignatureReceipt)
 
 	router.Group(func(admin chi.Router) {
 		admin.Use(a.authenticated)
