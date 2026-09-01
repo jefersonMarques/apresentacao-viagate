35af4a720b544994d068c2c7116ac99b3303b56a
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-09-01 09:17:19 -0300

    remove obsolete static and Supabase legacy

--- proposal/config.js
+++
@@ -1,8 +0,0 @@
-export const proposalConfig = Object.freeze({
-  supabaseUrl: 'https://orxenaynumsrohvsywxb.supabase.co',
-  supabasePublishableKey: 'sb_publishable_HUUvFXqWLb5ApCmi21uuWQ_nnirYlZd',
-  publicProposalUrl: './view.html',
-  publicPresentationUrl: '../view.html',
-  loginUrl: './',
-  assetBucket: 'commercial-assets',
-});
