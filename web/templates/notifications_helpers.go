5bd277aaf6873ab29639dd67c332ba49e134b2f9
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-09-01 11:54:14 -0300

    format notification badge count

--- web/templates/notifications_helpers.go
+++ web/templates/notifications_helpers.go
@@ -1,10 +1,21 @@
 package templates
 
-import "github.com/jefersonMarques/apresentacao-viagate/internal/domain"
+import (
+	"strconv"
+
+	"github.com/jefersonMarques/apresentacao-viagate/internal/domain"
+)
 
 func NotificationRowClass(item domain.InAppNotification) string {
 	if item.ReadAt == nil {
 		return "admin-notification-row is-unread"
 	}
 	return "admin-notification-row"
+}
+
+func NotificationCount(total int) string {
+	if total > 99 {
+		return "99+"
+	}
+	return strconv.Itoa(total)
 }
